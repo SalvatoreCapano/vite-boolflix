c6f7ae57e3cb23234587826e23631038d2e8ba36
Salvatore Capano <capano_salvatore@libero.it>
2023-02-08 18:11:25 +0100

    Milestone 2 Completata

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,34 @@
   data () {
     return {
       searchQuery: "",
-      result: ""
+      resultMovies: "",
+      resultTVShows: ""
+    }
+  },
+  methods: {
+    getFlag (country) {
+      console.log("Sono in getFlag", country);
+      if (country == "en") {
+        return new URL("./assets/commonFlags/flag_USA.png", import.meta.url).href;
+      }
+      else {
+        return `https://flagsapi.com/${country.toUpperCase()}/flat/64.png`;
+      }
     }
   },
   computed: {
-    search() {
+    searchMovies() {
       axios.get(`https://api.themoviedb.org/3/search/movie?api_key=c5eed5002b0aa99da4cba9755d0e6251&query=${this.searchQuery}`)
           .then((response) => {
-            // this.result.push(response.data.results);
-            this.result = response.data.results;
-            console.log("Risultati", response.data.results[1].title);
-            console.log("Result", this.result);
+            this.resultMovies = response.data.results;
+            console.log("Result Movies", this.resultMovies);
+          });
+    },
+    searchTVShows() {
+      axios.get(`https://api.themoviedb.org/3/search/tv?api_key=c5eed5002b0aa99da4cba9755d0e6251&query=${this.searchQuery}`)
+          .then((response) => {
+            this.resultTVShows = response.data.results;
+            console.log("Result TV Shows", this.resultTVShows);
           });
     }
   }
@@ -29,25 +46,49 @@
   
   <div>
     <input type="text" v-model="searchQuery">
-    <button @click="search">cerca</button>
+    <button @click="searchMovies, searchTVShows">cerca</button>
   </div>
 
   <hr>
 
-  <div>
-    <div class="movie" v-for="movie in this.result">
-      <p>Titolo: {{ movie.title }}</p>
-      <p>Titolo Originale: {{ movie.original_title }}</p>
-      <p>Linuga: {{ movie.original_language }}</p>
-      <p>Voto: {{ movie.vote_average }}</p>
-    </div>
-  </div>
+  <main>
+    <!-- Movies -->
+    <section>
+      <div class="elem" v-for="movie in this.resultMovies">
+        <p>Titolo: {{ movie.title }}</p>
+        <p>Titolo Originale: {{ movie.original_title }}</p>
+        <p>Voto: {{ movie.vote_average }}</p>
+        <p>Linuga: {{ movie.original_language }}</p>
+        <img :src="getFlag(movie.original_language)" :alt="`lang: ${movie.original_language}`">
+      </div>
+    </section>
+
+    <!-- TV Shows -->
+    <section>
+      <div class="elem" v-for="show in this.resultTVShows">
+        <p>Titolo: {{ show.name }}</p>
+        <p>Titolo Originale: {{ show.original_name }}</p>
+        <p>Voto: {{ show.vote_average }}</p>
+        <p>Lingua Originale: {{ show.original_language }}</p>
+        <img :src="getFlag(show.original_language)" :alt="`lang: ${show.original_language}`">
+      </div>
+    </section>
+  </main>
+
 
 </template>
 
 <style >
-  .movie {
-    border: 1px solid red;
+main {
+  display: flex;
+}
+  .elem {
+    border: 2px solid red;
     margin-bottom: 1rem;
+    margin-right: 2rem;
+  }
+
+  img {
+    width: 50px;
   }
 </style>
